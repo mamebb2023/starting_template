8b010174f7fc7450c1c006170b23f78fa9f30ec9
Mohammednur <mamebb2023@gmail.com>
2024-09-14 10:20:22 +0300

    Remove unused SCSS styles and update JSX code

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,7 @@
-import style from "./page.module.scss";
-
 export default function Home() {
   return (
-    <main className={style.main}>
-      <h1 className={`${style.title} h1`}>Hello World!</h1>
+    <main className="">
+      <h1 className="">Hello World!</h1>
     </main>
   );
 }
